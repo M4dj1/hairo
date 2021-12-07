2bf7569de5f079f439882fbc4a69715be6b1e53a
Abdou Madji <a.madji@esi-sba.dz>
2021-12-07 13:58:48 +0100

    Implementing gender

--- lib/LoginRegScreens/password.dart
+++ lib/LoginRegScreens/password.dart
@@ -1,5 +1,7 @@
 import 'package:hairo/main.dart';
 import 'package:flutter/material.dart';
+import 'package:gender_picker/source/enums.dart';
+import 'package:gender_picker/gender_picker.dart';
 
 import '../home.dart';
 
@@ -17,10 +19,12 @@ class _PasswordState extends State<PasswordScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
+  final TextEditingController _genderController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     _phoneController.text = (widget.phone);
+    var gendController = "";
     return Scaffold(
       key: _scaffoldkey,
       body: SingleChildScrollView(
@@ -42,7 +46,7 @@ class _PasswordState extends State<PasswordScreen> {
                 padding:
                     const EdgeInsets.only(right: 30.0, left: 30.0, top: 40.0),
                 child: TextField(
-                  style: TextStyle(color: Colors.white),
+                  style: TextStyle(color: Colors.orangeAccent),
                   cursorColor: Colors.orangeAccent,
                   keyboardType: TextInputType.text,
                   controller: _phoneController,
@@ -77,6 +81,29 @@ class _PasswordState extends State<PasswordScreen> {
                     )),
               ),
             ),
+            Padding(
+                padding:
+                    const EdgeInsets.only(right: 30.0, left: 30.0, top: 40.0),
+                child: GenderPickerWithImage(
+                  onChanged: (Gender? gender) {
+                    gendController = gender.toString().substring(7);
+                  },
+                  showOtherGender: false,
+                  verticalAlignedText: false,
+                  selectedGender: Gender.Male,
+                  selectedGenderTextStyle: TextStyle(
+                      color: Colors.deepOrangeAccent.shade100,
+                      fontWeight: FontWeight.bold),
+                  unSelectedGenderTextStyle: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontWeight: FontWeight.normal),
+                  equallyAligned: true,
+                  animationDuration: Duration(milliseconds: 300),
+                  isCircular: true,
+                  opacityOfGradient: 0.4,
+                  padding: const EdgeInsets.all(3),
+                  size: 50,
+                )),
             Padding(
               padding: const EdgeInsets.only(
                   right: 30.0, left: 30.0, top: 30.0, bottom: 40),
@@ -123,6 +150,7 @@ class _PasswordState extends State<PasswordScreen> {
                     "mobile": widget.phone,
                     "password": _passwordController.text,
                     "name": _nameController.text,
+                    "gender": gendController,
                   };
 
                   dbRef.child(widget.uid).set(userDetails).then((value) {
