66aee4ab58564cef1d3b38479801bea3224eee7c
M4dj1 <a.madji@esi-sba.dz>
2021-12-08 00:24:56 +0100

    Check if user is logged in in Main then choosing which screen to go.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-
 import 'LoginRegScreens/loginpage.dart';
+import 'home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +20,30 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(scaffoldBackgroundColor: const Color(0x7D7D7D7D)),
-      home: LoginScreen(),
+      home: IsSignedIn(),
     );
+  }
+}
+
+class IsSignedIn extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    var name;
+
+    if (currentUser != null) {
+      dbRef
+          .child(currentUser.uid)
+          .child("name")
+          .once()
+          .then((DataSnapshot data) {
+        name = data.value;
+        print(currentUser.uid.toString());
+        print(name.toString());
+      });
+      return Home(currentUser.uid, name.toString());
+    } else {
+      return LoginScreen();
+    }
   }
 }
