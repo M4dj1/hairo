0462cd0ce2ea0e668761df9460e469a30dde3b03
Abdou Madji <a.madji@esi-sba.dz>
2021-12-08 11:11:05 +0100

    Checking whether the user is logged in or not and direct him to the right screen.

--- lib/LoginRegScreens/password.dart
+++ lib/LoginRegScreens/password.dart
@@ -19,7 +19,6 @@ class _PasswordState extends State<PasswordScreen> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
   final TextEditingController _phoneController = TextEditingController();
-  final TextEditingController _genderController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'LoginRegScreens/loginpage.dart';
-import 'home.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,30 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(scaffoldBackgroundColor: const Color(0x7D7D7D7D)),
-      home: IsSignedIn(),
+      home: LoginScreen(),
     );
-  }
-}
-
-class IsSignedIn extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final currentUser = FirebaseAuth.instance.currentUser;
-    var name;
-
-    if (currentUser != null) {
-      dbRef
-          .child(currentUser.uid)
-          .child("name")
-          .once()
-          .then((DataSnapshot data) {
-        name = data.value;
-        print(currentUser.uid.toString());
-        print(name.toString());
-      });
-      return Home(currentUser.uid, name.toString());
-    } else {
-      return LoginScreen();
-    }
   }
 }
