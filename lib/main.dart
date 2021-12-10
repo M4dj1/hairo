65b6712a85cdc6bd7ecd4d89efa2364e57646458
M4dj1 <a.madji@esi-sba.dz>
2021-12-10 12:42:45 +0100

    Checking currentUser and redirecting done -SharedPreference- Main

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,27 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:hairo/LoginRegScreens/loginpage.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../home.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  var uid = prefs.getString('uid');
+  if (uid != null) {
+    dbRef.child(uid).once().then((DataSnapshot snapshot) {
+      Map<dynamic, dynamic> userValues = snapshot.value;
+      runApp(MaterialApp(
+        home: Home(userValues),
+        theme: ThemeData(scaffoldBackgroundColor: Color(0xff051821)),
+      ));
+    });
+  } else {
+    runApp(MaterialApp(
+      home: LoginScreen(),
+      theme: ThemeData(scaffoldBackgroundColor: Color(0xff051821)),
+    ));
+  }
 }
 
 DatabaseReference dbRef = FirebaseDatabase.instance.reference().child("users");
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(scaffoldBackgroundColor: Color(0xff051821)),
-      home: LoginScreen(),
-    );
-  }
-}
