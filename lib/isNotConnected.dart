f5f0ab3388b71ab3abd7d0dbae5b1898c1685cfa
M4dj1 <a.madji@esi-sba.dz>
2021-12-12 23:50:57 +0100

    check connectivity and isNotConnected screen

---
+++ lib/isNotConnected.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+
+class IsNotConnected extends StatefulWidget {
+  const IsNotConnected({Key? key}) : super(key: key);
+
+  @override
+  _IsNotConnectedState createState() => _IsNotConnectedState();
+}
+
+class _IsNotConnectedState extends State<IsNotConnected> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+              height: 100,
+              width: 100,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/images/logo.png',
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              )),
+          Padding(
+            padding: EdgeInsets.only(bottom: 30),
+            child: DefaultTextStyle(
+              style: TextStyle(
+                  fontSize: 30.0,
+                  color: Colors.grey.shade200,
+                  fontFamily: 'Horizon'),
+              child: AnimatedTextKit(
+                totalRepeatCount: 1,
+                pause: Duration(milliseconds: 0),
+                repeatForever: true,
+                animatedTexts: [
+                  WavyAnimatedText('H a i r O',
+                      speed: Duration(milliseconds: 1000)),
+                ],
+                isRepeatingAnimation: true,
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.center,
+            child: SizedBox(
+              width: 250.0,
+              child: DefaultTextStyle(
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  color: Colors.red,
+                  fontSize: 20.0,
+                  fontFamily: 'Agne',
+                ),
+                child: AnimatedTextKit(
+                  totalRepeatCount: 1,
+                  isRepeatingAnimation: false,
+                  animatedTexts: [
+                    TypewriterAnimatedText(
+                      'Please check your internet connection and try again',
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                  onTap: () {
+                    print("Tap Event");
+                  },
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,25 +2,37 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:hairo/LoginRegScreens/loginpage.dart';
+import 'package:hairo/isNotConnected.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import '../home.dart';
+import 'dart:io';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  var mobile = prefs.getString('mobile');
-  if (mobile != null) {
-    dbRef.child(mobile).once().then((DataSnapshot snapshot) {
-      Map<dynamic, dynamic> userValues = snapshot.value;
-      runApp(MaterialApp(
-        home: Home(userValues),
-        theme: ThemeData(scaffoldBackgroundColor: Color(0xff051821)),
-      ));
-    });
-  } else {
+  try {
+    final result = await InternetAddress.lookup('google.com');
+    if (result.isNotEmpty && result[0].rawAddress.isNotEmpty) {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      var mobile = prefs.getString('mobile');
+      if (mobile != null) {
+        dbRef.child(mobile).once().then((DataSnapshot snapshot) {
+          Map<dynamic, dynamic> userValues = snapshot.value;
+          runApp(MaterialApp(
+            home: Home(userValues),
+            theme: ThemeData(scaffoldBackgroundColor: Color(0xff051821)),
+          ));
+        });
+      } else {
+        runApp(MaterialApp(
+          home: LoginScreen(),
+          theme: ThemeData(scaffoldBackgroundColor: Color(0xff051821)),
+        ));
+      }
+    }
+  } on SocketException catch (_) {
     runApp(MaterialApp(
-      home: LoginScreen(),
+      home: IsNotConnected(),
       theme: ThemeData(scaffoldBackgroundColor: Color(0xff051821)),
     ));
   }
